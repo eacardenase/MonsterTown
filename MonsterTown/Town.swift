84cb1980ad58cf0d99ff2113e30c008cf824179f
Edwin Cardenas <ecardenas@lwolf.com>
2025-03-29 17:17:44 -0500

    feat: nested types and readonly properties aka constants

--- MonsterTown/Town.swift
+++ MonsterTown/Town.swift
@@ -8,8 +8,13 @@
 import Foundation
 
 struct Town {
+    let region = "Middle"
     var population = 5_422
     var numberOfSpotlights = 4
+    
+    enum Size {
+        case small, medium, large
+    }
     
     func printDescription() {
         print("Population: \(population)\nNumber of spotlights: \(numberOfSpotlights)")
