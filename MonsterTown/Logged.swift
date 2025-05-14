ae1e1c1c349c70911e9a48cbf4ee9ec5c0620e73
Edwin Cardenas <ecardenas@lwolf.com>
2025-05-14 18:28:59 -0500

    feat: remove warningValue references

--- MonsterTown/Logged.swift
+++ MonsterTown/Logged.swift
@@ -9,7 +9,7 @@ import Foundation
 
 @propertyWrapper struct Logged {
     private var storage: Int
-    private var warningValue: Int
+//    private var warningValue: Int
     
     var wrappedValue: Int {
         set {
@@ -17,17 +17,17 @@ import Foundation
             
             storage = newValue
             
-            if storage < warningValue {
-                print("\(newValue) is getting too low.")
-            }
+//            if storage < warningValue {
+//                print("\(newValue) is getting too low.")
+//            }
         }
         get {
             return storage
         }
     }
     
-    init(wrappedValue: Int, warningValue: Int) {
+    init(wrappedValue: Int, /*warningValue: Int*/) {
         storage = wrappedValue
-        self.warningValue = warningValue
+//        self.warningValue = warningValue
     }
 }
